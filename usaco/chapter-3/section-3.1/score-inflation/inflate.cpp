7aaad41f8e3687f13a5eb2d8cebafa79446ed427
louis <hjingjia@gmail.com>
2015-09-08 18:02:36 +0800

    score inflation I

---
+++ usaco/chapter-3/section-3.1/score-inflation/inflate.cpp
@@ -0,0 +1,81 @@
+/*
+  ID: come_1b1
+  LANG: C++11
+  TASK: inflate
+ */
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+
+const int max_v = 10000+10;
+//int data[max_v][max_v];
+
+int M,N;
+
+struct Cls{
+  int p;
+  int m;
+}cls[max_v];
+#if 0
+void print(){
+  for( int i = 0; i < N; ++i ){
+    std::copy( std::begin( data[i] ),
+	       std::begin( data[i] ) + M,
+	       std::ostream_iterator<int>( std::cout, " " ) );
+    std::cout << std::endl;
+  }
+}
+#endif
+
+
+int solve( int m, int n ){
+  int ret = -1;
+  if( m < 0  ){
+    return -1;
+  }
+  int ret1,ret2;
+    
+  if( n==0 ){
+    if( m < cls[n].m )
+      ret =  -1;
+    else
+    //data[n][m] = m / cls[n].m * cls[n].p;
+    ret = m / cls[n].m * cls[n].p;
+  }else {
+  
+    //    if( data[n-1][m] == 0 )
+      ret1 = solve( m, n - 1 );
+
+      //    if( data[n][m-cls[n].m] == 0 )
+      ret2 = solve( m - cls[n].m, n );
+  
+    //    data[n][m] = std::max( data[n-1][m],
+    //			   data[n][m-cls[n].m] >=0 ?data[n][m-cls[n].m] + cls[n].p:0);
+      ret = std::max( ret1, ret2 >= 0 ? (ret2 + cls[n].p ): 0 );
+  }
+
+  //  std::cout << n << ":" << m << " " << data[n][m] << std::endl;
+  //  return data[n][m];
+  //  std::cout << m << " " << n << ":" << ret1 << " " << ret2 << " " << ret << std::endl;
+  return ret;
+}
+
+int main(){
+  std::ifstream fin("inflate.in");
+  fin >> M >> N;
+  for( int i = 0; i < N; ++i )
+    fin >> cls[i].p >> cls[i].m;
+  fin.close();
+
+  //  print();
+  std::ofstream fout("inflate.out");
+  fout << solve( M, N-1 ) << std::endl;
+  fout.close();
+
+  // std::cout << solve( 20, 3 ) << std::endl;
+
+  return 0;
+ 
+}
